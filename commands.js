2e44e1f1f7fcc2f4659f6ac3ad99222ca71da4ef
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-12-08 11:12:51 -0800

    add jaskis prompts

---
+++ commands.js
@@ -0,0 +1,46 @@
+// JASKIS 
+// paste the MongoDB commands you use underneath each prompt 
+
+// GETTING STARTED 
+// 1. Create a database called jaskis 
+
+
+// 2. Create a collection called bounties
+
+
+// ADD THE ANIMAL BOUNTIES 
+// 1. Insert the given "Thanoceros" bounty object 
+
+
+// 2. Query for all bounties in the bounties collection 
+
+
+// 3. Insert many bounties at once using the given objects 
+
+
+// MANAGE THE DATABASE 
+// Queries
+// 1. Query for all bounties in the Grasslands 
+
+
+// 2. Query for all bounties with a reward worth 10000 or more 
+
+
+// 3. Query for all bounties, but exclude the client attribute from being shown 
+
+
+// 4. Query for a Groundhog in the Woodlands 
+
+
+// Update and Delete 
+// 1. Update the reward for Polarwind to 10000 
+
+
+// 2. Remove Lokinkajou 
+
+
+// 3. Delete all bounties sent by Songbird 
+
+
+// 4. Update all captured statuses to true 
+
